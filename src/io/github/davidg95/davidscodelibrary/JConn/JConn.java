63fdd206caede2901fb7ea45cebf70539fd10cf2
BuildTools <unconfigured@null.spigotmc.org>
2017-06-21 16:54:57 +0100

    - Javadoc

--- src/io/github/davidg95/davidscodelibrary/JConn/JConn.java
+++ src/io/github/davidg95/davidscodelibrary/JConn/JConn.java
@@ -11,6 +11,7 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
+ * Class for sending JConn requests to a JConn server.
  *
  * @author David
  */
--- src/io/github/davidg95/davidscodelibrary/JConn/JConnRunnable.java
+++ src/io/github/davidg95/davidscodelibrary/JConn/JConnRunnable.java
@@ -6,10 +6,17 @@
 package io.github.davidg95.davidscodelibrary.JConn;
 
 /**
+ * Interface which is used for specifying the method to be executed when a reply
+ * is sent back.
  *
  * @author David
  */
 public interface JConnRunnable {
-    
+
+    /**
+     * This method gets run on a reply from the server.
+     *
+     * @param reply the JConnData object from the server.
+     */
     public void run(JConnData reply);
 }
